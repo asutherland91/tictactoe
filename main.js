781e4935cd3b4a8ed9f65b770358f873196bb50e
Adriane Sutherland <asutherland91@gmail.com>
2023-03-01 12:28:02 -0800

    Add some query selectors

---
+++ main.js
@@ -0,0 +1,34 @@
+//query selectors
+var nameInputOne = document.querySelector("#name-input1");
+var nameInputTwo = document.querySelector("#nameInput2");
+var radioMario = document.querySelector("#mario");
+var radioLuigi = document.querySelector("#luigi");
+var radioPeach = document.querySelector("#peach");
+var radioYoshi = document.querySelector("#yoshi");
+var radioBowser = document.querySelector("#bowser");
+var radioBoo = document.querySelector("#boo");
+var radioGoomba = document.querySelector("#goomba");
+var radioSpiny = document.querySelector("#spiny");
+var letsGoButton = document.querySelector(".submit");
+var leftOne = document.querySelector(".left-1");
+var midOne = document.querySelector(".mid-1");
+var RightOne = document.querySelector(".right-1");
+var leftTwo = document.querySelector(".left-2");
+var midTwo = document.querySelector(".mid-2");
+var rightTwo = document.querySelector(".right-2");
+var leftThree = document.querySelector(".left-3");
+var midThree = document.querySelector(".mid-3");
+var rightThree = document.querySelector(".right-3");
+
+
+
+
+
+
+//event listeners
+
+
+
+
+
+//functions
