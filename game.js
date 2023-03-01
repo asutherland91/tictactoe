42c12f7f64abb6431087a16b509b19ccfb272262
Adriane Sutherland <asutherland91@gmail.com>
2023-02-28 18:06:14 -0800

    Add basic outlining of functions for game.js file.

---
+++ game.js
@@ -0,0 +1,19 @@
+class Game {
+  constructor(player1, player2) {
+    this.gameWon = false;
+    this.gameDraw = false;
+  }
+
+  whoseTurn() {
+
+  }
+
+  gameState() {
+    if(player.token)
+  }
+
+  resetGame() {
+
+  }
+
+}
